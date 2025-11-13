7efceda940eb2785aeb04f2ca3d67c878604f42e
Ben Weibel <weibel.b@gmail.com>
2025-11-12 21:47:01 -0800

    Set Default header with minimal styling from the editor

---
+++ patterns/header.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: Header
+ * Slug: ollie/header
+ * Description: Header with nav and social icons
+ * Categories: header
+ * Keywords: header, nav, links, button
+ * Viewport Width: 1500
+ * Block Types: core/template-part/header
+ * Post Types: wp_template
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"tagName":"header","metadata":{"name":"Site Header"},"className":"site-header","layout":{"type":"constrained"}} -->
+<!-- wp:site-logo /-->
+
+<!-- wp:navigation {"ref":12,"overlayMenu":"never","className":"getdave-responsive-navigation-block-is-desktop"} /-->
+<!-- /wp:group -->
